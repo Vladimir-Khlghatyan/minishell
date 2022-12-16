34484ca4986a736623f22a3ca1345c638c1ff227
Vladimir <94677889+Vladimir-Khlghatyan@users.noreply.github.com>
2022-12-17 00:18:37 +0400

    color code upd

--- includes/defines.h
+++ includes/defines.h
@@ -13,13 +13,13 @@
 #ifndef DEFINES_H
 # define DEFINES_H
 
-# define RED       		"\33[1;31m"
-# define GREEN      	"\33[1;32m"
-# define PINK	    	"\33[1;35m"
+# define RED			"\33[1;31m"
+# define GREEN			"\33[1;32m"
 # define YELLOW			"\33[1;33m"
-# define BLUE     		"\33[1;36m"
-# define CYAN       	"\33[1;37m"
-# define WHITE			"\033[1;37m"
+# define BLUE			"\33[1;34m"
+# define PINK			"\33[1;35m"
+# define CYAN			"\33[1;36m"
+# define WHITE			"\33[1;37m"
 # define RESET			"\33[0;m"
 
 # define MC 			" |<>"
